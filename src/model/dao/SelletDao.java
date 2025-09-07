418fd4765f48f42935f4afa638e9281c9f6503d7
Leonardo250 <leomoulin2002@gmail.com>
2025-09-07 00:29:33 -0300

    Interfaces de Department e Seller criadas

---
+++ src/model/dao/SelletDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import model.entities.Sellet;
+
+import java.util.List;
+
+public interface SelletDao {
+    void insert(Sellet obj);
+    void update(Sellet obj);
+    void DeleteById(Integer id );
+    Sellet findById(Integer id);
+    List<Sellet> findAll();
+}
+
